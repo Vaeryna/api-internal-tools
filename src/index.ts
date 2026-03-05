c743c97a958d5c47a797af058b9a842cffc4eff4
Melanie <leblond.melanie@gmail.com>
2026-03-05 12:13:33 +0100

    installion fastify + pose base serveur

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
-function ApiInternalTools() {
-    console.log("pouet")
-}
+const fastify = require('fastify')({
+    logger: true
+})
 
+// Declare a route
+fastify.get('/', function (request: any, reply: { send: (arg0: { hello: string }) => void }) {
+    reply.send({hello: 'world'})
+})
 
-ApiInternalTools()
+// Run the server!
+fastify.listen({port: 3000}, function (err: any, address: any) {
+    console.log("serveur lancé !")
+    if (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+    // Server is now listening on ${address}
+})
