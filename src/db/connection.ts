9878399f176b8cd9405f315966cfe0a2982954f9
Melanie <leblond.melanie@gmail.com>
2026-03-06 10:12:29 +0100

    ajout d'un helper pour ne faciliter les requêtes DB

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,8 +1,21 @@
-import {Pool} from "pg";
+import {Pool, QueryResultRow} from "pg";
 
 
 const connectionString = process.env.DATABASE_URL
 if (!connectionString) throw new Error("DATABASE_URL is missing")
+
+
+export async function queryOne<T extends QueryResultRow>(sql: string
+    , params: any[]): Promise<T | null> {
+    const result = await pool.query<T>(sql, params)
+    return result.rows[0] ?? null
+}
+
+export async function queryMany<T extends QueryResultRow>(sql: string, params: any[]): Promise<T[] | null> {
+    const result = await pool.query<T>(sql, params)
+    return result.rows
+}
+
 
 export const pool = new Pool({connectionString})
 
